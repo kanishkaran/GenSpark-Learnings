b16ce6daf7118cd696c3cd88cec334a790896446
kanishkaran <kanishkaran456@gmail.com>
2025-05-16 18:28:10 +0530

    Add comments

--- Day-10-May16/Task_Data_Entry_User.sql
+++ Day-10-May16/Task_Data_Entry_User.sql
@@ -3,7 +3,7 @@
 --    * Can `INSERT` into `students`, `enrollments`
 --    * Cannot modify certificates directly
 
-
+-- Have access to Insert :)
 INSERT INTO tbl_students (student_name, email, st_phone_number, college_id) VALUES 
 ('Alex John', 'alex.john@example.com', 8076543210, 3),
 ('Boby Zane', 'bob.zane@example.com', 70056789, 1);
@@ -13,4 +13,5 @@ INSERT INTO tbl_enrollment (student_id, course_id, iscomplete) VALUES
 (4, 1, FALSE),
 (5, 3, TRUE);
 
+-- Does Not Have access to Update :<
 UPDATE tbl_certificates SET cert_id = 1 WHERE cert_id = 1;
