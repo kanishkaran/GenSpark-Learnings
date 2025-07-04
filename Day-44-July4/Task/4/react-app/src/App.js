f364378e7116abc211285faeb04cbea41e63159b
kanishkaran <kanishkaran456@gmail.com>
2025-07-04 18:39:21 +0530

    .

--- Day-44-July4/Task/4/react-app/src/App.js
+++ Day-44-July4/Task/4/react-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('Loading...');
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const res = await fetch('/hello');
+        console.log(res)
+        if (!res.ok) throw new Error('Network response was not ok');
+        const data = await res.text();
+        setMessage(data);
+      } catch (error) {
+        console.error('Fetch error:', error);
+        setMessage('Failed to fetch from backend');
+      }
+    };
+
+    fetchMessage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>React Frontend 🥳</h1>
+      <p>{message}</p>
     </div>
   );
-}
+};
 
 export default App;
